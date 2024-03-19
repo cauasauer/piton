bb0bc52e6e1308495fd583359b807aaf55ee8f23
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-03-19 08:48:20 -0300

    Update jokenpo.py

--- jokenpo.py
+++ jokenpo.py
@@ -1,9 +1,23 @@
 import random
- def jogar_jokenpo():
+
+def determine_vencedor(escolha_jogador, escolha_computador):
+    if escolha_jogador == escolha_computador:
+            return "Empate!"
+    elif (
+            (escolha_jogador == "papel" and escolha_computador == "pedra") or
+            (escolha_jogador == "pedra" and escolha_computador == "tesoura")or
+            (escolha_jogador == "tesoura" and escolha_computador == "pepel")
+        ):
+         return "Você venceu!"
+    else:
+         return "Você perdeu!"
+
+def jogar_jokenpo():
     opcoes = ["pedra", "papel","tesoura"]
     print("Bem-vindo ao jogo Jokenpô!")
     print("Escolha: pedra, papel ou tesoura.")
-
+              
+ 
     while True:
         escolha_jogador = input("Sua escolha:")
         if escolha_jogador not in opcoes:
@@ -13,6 +27,18 @@ import random
         escolha_computador = random.choice(opcoes)
 
         print(f"computaor escolheu: {escolha_computador}")
+
+        resultado = determine_vencedor(escolha_jogador,escolha_computador)
+        print(resultado)
+
+        if resultado == "Você ganhou!":
+            vitorias += 1
+        elif resultado == "Você perdeu!":
+            vitorias -= 1 
+        else:
+            vitorias += 0 
+
+        print(f"computador escolha")          
 
         if escolha_jogador == escolha_computador:
             print("Empate")
@@ -28,6 +54,9 @@ import random
         jogar_novamente = input("Você quer jogar novamente?")
         if jogar_novamente != "sim":
             break                    
+
+
+
 
 if __name__ == "__main__":
     jogar_jokenpo()
