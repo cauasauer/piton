640d679b7f08dc0edc5c73bb0069938762ac7e6c
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-03-26 11:39:44 -0300

    Add files via upload

---
+++ atividade phyton.py
@@ -0,0 +1,40 @@
+def inserir_item(lista):
+    novo_item = input("Digite o novo item: ")
+    lista.append(novo_item)
+    print("Item inserido com sucesso!")
+
+def excluir_item(lista):
+    item_a_excluir = input("Digite o item a excluir: ")
+    if item_a_excluir in lista:
+        lista.remove(item_a_excluir)
+        print("Item excluído com sucesso!")
+    else:
+        print("O item não está na lista.")
+
+def mostrar_lista(lista):
+    print("Lista atual:", lista)
+
+def main():
+    lista = []
+    menu = {
+        '1': inserir_item,
+        '2': excluir_item,
+        '3': mostrar_lista,
+        '4': lambda _: print("Programa encerrado.") 
+    }
+    
+    while True:
+        print("\n1. Inserir novo item na lista")
+        print("2. Excluir item da lista")
+        print("3. Mostrar lista atual")
+        print("4. Sair")
+        
+        escolha = input("\nEscolha uma opção: ")
+        
+        if escolha in menu:
+            menu[escolha](lista)
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
