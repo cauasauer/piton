423a9a6390ed899399c421afda7647746f0ec746
cauasauer <128698814+cauasauer@users.noreply.github.com>
2024-04-04 10:38:06 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -1,49 +1,79 @@
-def inserir_item(lista):
-    novo_item = input("Digite o novo item: ")
-    lista.append(novo_item)
-    print("Item inserido com sucesso!")
+import os
 
-def excluir_item(lista):
-    item_a_excluir = input("Digite o item a excluir: ")
-    if item_a_excluir in lista:
-        lista.remove(item_a_excluir)
-        print("Item excluído com sucesso!")
+def mostrarLista(lista):
+    print("Lista atual:", lista)
+
+def addItemLista(lista):
+    novo_item = input("Digite um novo item para adicionar na lista: ")
+    lista.append(novo_item)
+
+def excluirItemLista(lista):
+    itemExcluir = input("Digite o item para excluir da lista: ")
+    if itemExcluir in lista:
+        lista.remove(itemExcluir)
     else:
         print("O item não está na lista.")
 
-def mostrar_lista(lista):
-    print("Lista atual:", lista)
-    
-def gravar_lista(lista):
-    gravar_arq = input("Digite o nome do arquivo")
-    with open(gravar_arq,"w") as arquivo:
-        for item in lista:
-            arquivo.write(item + "\n")
-    print("GRAVADO",gravar_arq)        
+def mostrarListaAtual(lista):
+    mostrarLista(lista)
 
-def main():
-    lista = []
-    menu = {
-        '1': inserir_item,
-        '2': excluir_item,
-        '3': mostrar_lista,
-        '4': gravar_lista,
-        '5': lambda _: print("Programa encerrado.") 
-    }
-    
+def gravarLista(lista):
+    arquivo = input("Digite o nome do arquivo a ser gravado: ")
+    arquivo += ".txt" 
+    try:
+        with open(arquivo, 'w') as f:
+            f.write(str(lista))
+        print("Lista gravada com sucesso no arquivo", arquivo)
+    except Exception as e:
+        print("Erro ao gravar o arquivo:", e)
+
+def lerArquivoLista(extensao=".txt"):
+    arquivos = os.listdir()
+    print(f"arquivo .{extensao}Lista de Arquivos disponíveis:")
+    for arquivo in arquivos:
+        if arquivo.endswith(extensao):
+            print(arquivo)
+    arquivoEscolhido = input("Digite o nome do arquivo para ler: ")
+    try:
+        with open(arquivoEscolhido, 'r') as f:
+            lista = eval(f.read())
+            mostrarLista(lista)
+            return lista
+    except Exception as e:
+        print("Erro ao ler o arquivo:", e)
+
+def sair():
+    print("Saindo do programa...")
+    exit()
+
+def escolherOpcao(lista):
     while True:
-        print("\n1. Inserir novo item na lista")
-        print("2. Excluir item da lista")
-        print("3. Mostrar lista atual")
-        print("4. gravar lista")
-        print("5. Sair")
-        
-        escolha = input("\nEscolha uma opção: ")
-        
-        if escolha in menu:
-            menu[escolha](lista)
+        print(" Escolha uma opção:")
+        print("1 - Adicionar item na lista")
+        print("2 - Excluir um item da lista")
+        print("3 - Mostrar lista")
+        print("4 - Gravar lista")
+        print("5 - Ler um arquivo de lista epecifico")
+        print("6 - Sair")
+
+        opc = input("Digite a opção desejada: ")
+
+        if opc == '1':
+            addItemLista(lista)
+        elif opc == '2':
+            excluirItemLista(lista)
+        elif opc == '3':
+            mostrarListaAtual(lista)
+        elif opc == '4':
+            gravarLista(lista)
+        elif opc == '5':
+            lista = lerArquivoLista()
+        elif opc == '6':
+            sair()
+            break
         else:
-            print("Opção inválida. Por favor, escolha uma opção válida.")
+            print("Opção inválida.")
 
 if __name__ == "__main__":
-    main()
+    lista = []
+    escolherOpcao(lista)
